a06358fe39310035f736b4f94d08a3fbf8795617
Araysh <89472193+Araysh@users.noreply.github.com>
2021-08-28 12:49:27 +0300

    Updated!

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-// Hex.js, Hex yor strings easily. Makes hard things easy.
-// Authored: 2017/12/21
-// Licene: MIT
+/**
+ * Hex.js is a module that turns strings into an hex key, and it could also reverse the process.
+ * Made by: Araysh with the help of Kylemods
+ */
 
 "use strict";
 
+function Hex() {
+  this.hex = (str) => {
+    var hex
 
-module.exports.hex = function(str, hex) {
-	try{
-    hex = unescape(encodeURIComponent(str))
-    .split('').map(function(v){
-      return v.charCodeAt(0).toString(16)
-    }).join('')
+    try {
+      hex = unescape(encodeURIComponent(str))
+      .split('').map((v) => {
+        return v.charCodeAt(0).toString(16)
+      }).join('')
+    } catch(e) {
+      hex = str
+      throw new TypeError(`Invalid text input: ${str}`)
+    }
+    return hex
   }
-  catch(e){
-    hex = str
-    throw new TypeError('invalid text input: ' + str)
+
+  this.unHex = (hex) => {
+    var str
+
+    try {
+      str = decodeURIComponent(hex.replace(/(..)/g,'%$1'))
+    } catch (e) {
+      throw new TypeError(`Invalid hex input: ${str}`)
+    }
+    return str
   }
-  return hex
 }
 
-module.exports.unHex = function(hex, str) {
-try{
-    str = decodeURIComponent(hex.replace(/(..)/g,'%$1'))
-  }
-  catch(e){
-    str = hex
-    throw new TypeError('invalid text input: ' + hex)
-  }
-  return str
-}
+let hexClient = new Hex();
+
+
+module.exports.hex = hexClient.hex;
+module.exports.unHex = hexClient.unHex;
